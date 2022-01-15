0628829b8e04647fdc385b1fd2067662b0ac19d3
AArkhipenko <arkhipenko.alex95@gmail.com>
2022-01-15 12:23:32 +0300

    Добавлены доменные модели для работы с пользователями

---
+++ src/General.Service.Domain/Models/User.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace General.Service.Domain.Models
+{
+    /// <summary>
+    /// Доменная модель пользователя
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// Конструктор <see cref="GetUserListQuery" />
+        /// </summary>
+        /// <param name="id">идентификатор записи</param>
+        /// <param name="name">имя пользователя</param>
+        /// <param name="surname">фамилия пользователя</param>
+        /// <param name="birthday">дата рождения</param>
+        public User(
+            int id,
+            string name,
+            string surname,
+            DateTime birthday)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Surname = surname;
+            this.Birthday = birthday;
+        }
+
+        /// <summary>
+        /// Идентификатор записи
+        /// </summary>
+        public int Id { get; }
+
+        /// <summary>
+        /// Имя пользователя
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Фамилия пользователя
+        /// </summary>
+        public string Surname { get; }
+
+        /// <summary>
+        /// Дата рождения
+        /// </summary>
+        public DateTime Birthday { get; }
+    }
+}
---
+++ src/General.Service.Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,23 @@
+﻿using General.Service.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace General.Service.Domain.Repositories
+{
+    /// <summary>
+    /// Интерфейс репозитория для работы с пользователями
+    /// </summary>
+    public interface IUserRepository
+    {
+        /// <summary>
+        /// Получение списка пользователей
+        /// </summary>
+        /// <param name="offset">смещение (в количестве записей)</param>
+        /// <param name="count">количество записей</param>
+        /// <returns>асинхронный список пользователей</returns>
+        IAsyncEnumerable<User> GetListAsync(int offset, int count);
+    }
+}
