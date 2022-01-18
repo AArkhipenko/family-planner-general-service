0e44bf048d917d78a9f85d832420cfce8aada737
AArkhipenko <arkhipenko.alex95@gmail.com>
2022-01-18 17:39:58 +0300

    Добавлены тесты

---
+++ test/General.Service.Api.Test/Controllers/V10/Types/TypesControllerTests.GetList.cs
@@ -0,0 +1,81 @@
+﻿using General.Service.Application.Types.DTO;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using Xunit;
+
+namespace General.Service.Api.Test.Controllers.V10
+{
+    public partial class TypesControllerTests
+    {
+        [Fact]
+        public async Task Get_types_list_by_code_has_not_errors()
+        {
+            // Arrage
+            var query = HttpUtility.ParseQueryString(string.Empty);
+            query["offset"] = "0";
+            query["count"] = "10";
+            query["code"] = "code1";
+
+            // Act
+            var response = await _client.GetAsync($"/type/v10/list_by_code?{query.ToString()}");
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            var result = await response.ReadJsonAsync<IList<TypeListDTO>>();
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
+            Assert.Equal(2, result.Count);
+        }
+
+        [Fact]
+        public async Task Get_types_list_by_code_has_offset_BagRequest_error()
+        {
+            // Arrage
+            var query = HttpUtility.ParseQueryString(string.Empty);
+            query["offset"] = "-1";
+            query["count"] = "10";
+            query["code"] = "code1";
+
+            // Act
+            var response = await _client.GetAsync($"/type/v10/list_by_code?{query.ToString()}");
+
+            // Assert
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Get_types_list_by_code_has_count_BagRequest_error()
+        {
+            // Arrage
+            var query = HttpUtility.ParseQueryString(string.Empty);
+            query["offset"] = "0";
+            query["count"] = "0";
+            query["code"] = "code1";
+
+            // Act
+            var response = await _client.GetAsync($"/type/v10/list_by_code?{query.ToString()}");
+
+            // Assert
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Get_types_list_by_code_has_code_BagRequest_error()
+        {
+            // Arrage
+            var query = HttpUtility.ParseQueryString(string.Empty);
+            query["offset"] = "0";
+            query["count"] = "0";
+            query["code"] = string.Empty;
+
+            // Act
+            var response = await _client.GetAsync($"/type/v10/list_by_code?{query.ToString()}");
+
+            // Assert
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+    }
+}
